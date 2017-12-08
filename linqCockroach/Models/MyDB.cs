ff4e9afc0cbec5519c50ba28c8bf1a534324ba8c
WaToI <gemini_3ma@hotmail.com>
2017-12-08 23:10:43 +0900

    linq2db Cockroach(PostgreSQL)

---
+++ linqCockroach/Models/MyDB.cs
@@ -0,0 +1,45 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: WaTo
+ * Date: 2017/12/08
+ * Time: 20:26
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using LinqToDB;
+using LinqToDB.Mapping;
+
+namespace linqCockroach.Models
+{
+	/// <summary>
+	/// Description of MyClass.
+	/// </summary>
+	public partial class MyDB : LinqToDB.Data.DataConnection
+	{
+		
+		public ITable<accounts> accounts { get { return this.GetTable<accounts>(); } }
+
+		public MyDB()
+		{
+		}
+
+		public MyDB(string configuration)
+			: base(configuration)
+		{
+		}
+	}
+	
+	[Table(Schema="bank", Name="accounts")]
+	public partial class accounts {
+		[PrimaryKey, NotNull] public int id {get;set;}
+		[Column, Nullable] public decimal balance {get;set;}
+		
+		public override string ToString()
+		{
+			return string.Format("[accounts Id={0}, Balance={1}]", id, balance);
+		}
+
+	}
+}
---
+++ linqCockroach/Tests/MyDB_test.cs
@@ -0,0 +1,41 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: WaTo
+ * Date: 2017/12/08
+ * Time: 21:28
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using linqCockroach.Models;
+using Npgsql;
+using NUnit.Framework;
+
+namespace linqCockroach.Tests
+{
+	[TestFixture]
+	public class MyDB_test
+	{
+		[Test]
+		public void TestConnect()
+		{
+			using(var db = new NpgsqlConnection("Server=localhost;Port=26257;User Id=maxroach;Database=bank")){
+				db.Open();
+				Console.WriteLine( db.GetSchema() );
+				Console.WriteLine("接続おっけー👌");
+				db.Close();
+			}
+		}
+		
+		[Test]
+		public void TestSelect(){
+			using(var db = new MyDB("cnstrPostgreSQL")){
+				
+				var find = db.accounts.FirstOrDefault();
+				Console.WriteLine(find.ToString());
+			}
+		}
+	}
+}
